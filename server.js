de6849ecf3ba7448052b8486b0a73e4b66ea1af9
arpitshukla267 <shuklaarpit440@gmail.com>
2025-11-08 13:42:33 +0530

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ✅ CORS setup
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"];
+const allowedOrigins = ["http://localhost:5173", "https://surpriselly.vercel.app", "http://localhost:3000"];
 app.use(
   cors({
     origin: function (origin, callback) {
